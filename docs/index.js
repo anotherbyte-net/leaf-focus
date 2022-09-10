43a897fd033aace6759ccf4ab749693b41a7ec25
Mark Cottman-Fields <cofiem@gmail.com>
2022-09-10 21:31:09 +1000

    docs: regenerate docs

--- docs/index.js
+++ docs/index.js
@@ -14,7 +14,7 @@ INDEX=[
 {
 "ref":"leaf_focus",
 "url":0,
-"doc":"Documentation for the leaf focus package.  leaf-focus Extract structured text from pdf files.  Install Install from PyPI using pip:   pip install leaf-focus   Download the [Xpdf command line tools](https: www.xpdfreader.com/download.html) and extract the executable files. Provide the directory containing the executable files as   exe-dir .  Usage   usage: leaf-focus [-h] [ version]  exe-dir EXE_DIR [ page-images] [ ocr] [ first FIRST] [ last LAST] [ log-level {debug,info,warning,error,critical}] input_pdf output_dir Extract structured text from a pdf file. positional arguments: input_pdf path to the pdf file to read output_dir path to the directory to save the extracted text files optional arguments: -h,  help show this help message and exit  version show program's version number and exit  exe-dir EXE_DIR path to the directory containing xpdf executable files  page-images save each page of the pdf as a separate image  ocr run optical character recognition on each page of the pdf  first FIRST the first pdf page to process  last LAST the last pdf page to process  log-level {debug,info,warning,error,critical} the log level: debug, info, warning, error, critical    Examples    Extract the pdf information and embedded text. leaf-focus  exe-dir [path-to-xpdf-exe-dir] file.pdf file-pages  Extract the pdf information, embedded text, an image of each page, and Optical Character Recognition results of each page. leaf-focus  exe-dir [path-to-xpdf-exe-dir] file.pdf file-pages  ocr    Change log  [v0.4.0](https: github.com/anotherbyte-net/leaf-focus/releases/tag/v0.4.0) - Implement wrapper for [keras-ocr](https: github.com/faustomorales/keras-ocr) - Implement wrapper for [xpdf tools](https: www.xpdfreader.com/about.html) - Implement initial cli using Python's argparse - Add docs and doc generation using [pdoc3](https: github.com/pdoc3/pdoc)"
+"doc":"Documentation for the leaf focus package.  leaf-focus Extract structured text from pdf files.  Install Install from PyPI using pip:   pip install leaf-focus   [![PyPI](https: img.shields.io/pypi/v/leaf-focus)](https: pypi.org/project/leaf-focus/) ![PyPI - Python Version](https: img.shields.io/pypi/pyversions/leaf-focus) ![GitHub Workflow Status (branch)](https: img.shields.io/github/workflow/status/anotherbyte-net/leaf-focus/Create%20Package/main) Download the [Xpdf command line tools](https: www.xpdfreader.com/download.html) and extract the executable files. Provide the directory containing the executable files as   exe-dir .  Usage   usage: leaf-focus [-h] [ version]  exe-dir EXE_DIR [ page-images] [ ocr] [ first FIRST] [ last LAST] [ log-level {debug,info,warning,error,critical}] input_pdf output_dir Extract structured text from a pdf file. positional arguments: input_pdf path to the pdf file to read output_dir path to the directory to save the extracted text files optional arguments: -h,  help show this help message and exit  version show program's version number and exit  exe-dir EXE_DIR path to the directory containing xpdf executable files  page-images save each page of the pdf as a separate image  ocr run optical character recognition on each page of the pdf  first FIRST the first pdf page to process  last LAST the last pdf page to process  log-level {debug,info,warning,error,critical} the log level: debug, info, warning, error, critical    Examples    Extract the pdf information and embedded text. leaf-focus  exe-dir [path-to-xpdf-exe-dir] file.pdf file-pages  Extract the pdf information, embedded text, an image of each page, and Optical Character Recognition results of each page. leaf-focus  exe-dir [path-to-xpdf-exe-dir] file.pdf file-pages  ocr    Change log  [v0.4.0](https: github.com/anotherbyte-net/leaf-focus/releases/tag/v0.4.0) - Implement wrapper for [keras-ocr](https: github.com/faustomorales/keras-ocr) - Implement wrapper for [xpdf tools](https: www.xpdfreader.com/about.html) - Implement initial cli using Python's argparse - Add docs and doc generation using [pdoc3](https: github.com/pdoc3/pdoc)"
 },
 {
 "ref":"leaf_focus.app",
@@ -105,9 +105,21 @@ INDEX=[
 "doc":"OCR implementation using keras-ocr."
 },
 {
-"ref":"leaf_focus.ocr.keras_ocr.OpticalCharacterRecognition.create_engine",
+"ref":"leaf_focus.ocr.keras_ocr.OpticalCharacterRecognition.engine_create",
 "url":4,
 "doc":"Create the OCR engine.",
+"func":1
+},
+{
+"ref":"leaf_focus.ocr.keras_ocr.OpticalCharacterRecognition.engine_run",
+"url":4,
+"doc":"",
+"func":1
+},
+{
+"ref":"leaf_focus.ocr.keras_ocr.OpticalCharacterRecognition.engine_annotate",
+"url":4,
+"doc":"",
 "func":1
 },
 {
